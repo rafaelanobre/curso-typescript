5e4c589881c539711fa0570d588c61a7ff20ada0
rafaelanobre <raafanobre@gmail.com>
2023-09-13 18:26:08 -0300

    Aula 5

---
+++ aula-ts-05/index.ts
@@ -0,0 +1,5 @@
+const numbers: number[] = [1,2,3,4,5];
+const anwsers: Array<boolean> = [true, false, false, true, false];
+const strings: string[] = ["one", "two", "three", "four", "five"];
+const mix: [number, string, number, string, number] = [1,"two",3,"four",5];
+const floatNumbers: Array<number> = [1.0,2.5,3.0,4.1,5.5];
